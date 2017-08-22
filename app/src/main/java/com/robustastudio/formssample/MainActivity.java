883f291bb7422fd8ff6c272c79a423302f10da4a
amrelmasry <amrelmasry.me@gmail.com>
2017-08-22 21:48:35 +0200

    Update sample app

--- app/src/main/java/com/robustastudio/formssample/MainActivity.java
+++ app/src/main/java/com/robustastudio/formssample/MainActivity.java
@@ -2,13 +2,45 @@ package com.robustastudio.formssample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.robustastudio.forms.utils.FormItemUtil;
+import com.robustastudio.forms.validation.FormValidator;
+import com.robustastudio.forms.widget.FormItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @NotEmpty
+    FormItem nameFormItem;
+    @NotEmpty
+    FormItem passwordFormItem;
+
+    private Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nameFormItem = (FormItem) findViewById(R.id.name_form_item);
+        passwordFormItem = (FormItem) findViewById(R.id.password_form_item);
+
+        validator = FormValidator.newInstance(this, new FormValidator.DefaultFormValidator() {
+            @Override
+            public void onValidationSucceeded() {
+                Toast.makeText(MainActivity.this, "Validation Succeeded", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FormItemUtil.removeErrorHintIfFound(nameFormItem, passwordFormItem);
+                validator.validate();
+            }
+        });
     }
 }
